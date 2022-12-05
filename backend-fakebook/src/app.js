07408495f6fa295525513b9eda0d7eb654e3c332
JohnnyDeBoas <johnnydeboas@gmail.com>
2022-12-05 01:47:41 -0300

    [REMOVE] remove cors

--- backend-fakebook/src/app.js
+++ backend-fakebook/src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const routes = require('./routes');
 require('./db/index');
 
@@ -12,7 +12,7 @@ class App{
 
     middlewares(){
         this.server.use(express.json())
-        this.server.use(cors())
+        // this.server.use(cors())
     }
 
     routes() {
